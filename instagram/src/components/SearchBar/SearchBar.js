fb2796e0b1df29cd105648659c02dddcba44bd14
Kevin Smith <keveightysev@gmail.com>
2019-02-18 15:18:57 -0600

    Created basic search bar component

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCompass, faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
+import logo from '../../assets/iglogo.png';
+
+import './SearchBar.css';
+
+const SearchBar = props => {
+    return (
+        <header>
+            <div className="logo">
+                <FontAwesomeIcon icon={faInstagram} />
+                <img src={logo} alt="Instagram logo"/>
+            </div>
+            <input type="search" />
+            <div className="icons">
+                <FontAwesomeIcon icon={faCompass} />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faUser} />
+            </div>
+        </header>
+    );
+}
+
+export default SearchBar;
