542774c8e81c22b6890d6a190b99ffdc602ef84e
Kevin Smith <keveightysev@gmail.com>
2019-02-20 14:12:57 -0600

    Set up authentication component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import authenticate from './components/Authentication/Authenticate';
 
 import PostsPage from './components/PostContainer/PostsPage'
 
 import './App.css';
 
 const App = () => {
+  const Page = authenticate(PostsPage);
   return (
-    <PostsPage />
+    <Page />
   );
 }
 
