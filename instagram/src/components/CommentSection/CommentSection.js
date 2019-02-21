ea43bedb0cecd50deb4c5c5af6f4800d5eb52f0f
Kevin Smith <keveightysev@gmail.com>
2019-02-21 14:37:28 -0600

    Transferred Comment Section styles to styled component

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,7 +3,46 @@ import moment from 'moment';
 import Comment from './Comment';
 import CommentForm from './CommentForm';
 
-import './CommentSection.css';
+import styled from 'styled-components';
+
+const CommentSectionStyle = styled.div`
+    margin: 0 10px;
+
+    p {
+        font-size: 1.5rem;
+        margin: 0 0 5px 0;
+    }
+
+    p.timestamp {
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        font-weight: 600;
+        color: lightgray;
+    }
+
+    form {
+        border-top: 1px solid lightgray;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        input {
+            width: 90%;
+            height: 30px;
+            border: none;
+            margin: 10px 0;
+
+            &::placeholder {
+                color: lightgray;
+                font-weight: 700;
+            }
+        }
+    }
+
+    svg {
+        font-size: 2rem;
+    }
+`;
 
 class CommentSection extends React.Component {
     constructor(props) {
@@ -35,7 +74,7 @@ class CommentSection extends React.Component {
 
     render() {
         return (
-            <div className="comment-section">
+            <CommentSectionStyle>
                 {this.state.comments.map((comment, index) => <Comment key={index} comment={comment} />)}
                 <p className="timestamp">{moment(this.props.data.timestamp, "MMMM Do YYYY, hh:mm:ss a").fromNow()}</p>
                 <CommentForm 
@@ -43,7 +82,7 @@ class CommentSection extends React.Component {
                     addComment={this.addComment}
                     inputComment={this.inputComment}  
                 />
-            </div>
+            </CommentSectionStyle>
         );
     }
 }
